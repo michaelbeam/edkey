162b60a8115fd791f5b2daebed301dc74621d2d5
michaeljbeam <michaeljbeam@gmail.com>
2018-07-31 16:53:00 -0500

    removed depricated code

--- edkey.go
+++ edkey.go
@@ -11,27 +11,6 @@ import (
 	ed "golang.org/x/crypto/ed25519"
 )
 
-var (
-	// PubKey Public Key
-	PubKey ed.PublicKey
-
-	// SecKey Secret Key
-	SecKey ed.PrivateKey
-)
-
-func init() {
-	if p, err := hex.DecodeString("fc2b5ecfd5ac1292bc39a393adfc1aeb76fc1d5bd5227737308523da370c5291"); err != nil {
-		log.Fatal(err)
-	} else {
-		PubKey = p
-	}
-	if s, err := hex.DecodeString("f1acb1538eb99067786e9a341bae5dea347a005329879b2204ae06512eb5b3effc2b5ecfd5ac1292bc39a393adfc1aeb76fc1d5bd5227737308523da370c5291"); err != nil {
-		log.Fatal(err)
-	} else {
-		SecKey = s
-	}
-}
-
 func main() {
 	c := cli.New()
 	c.HelpMsg = "edkey sign|verify|create"
